1060212fffea1f1ecd1b387636ce1383a15567b2
Ian Parkinson <ian_parkinson@live.co.uk>
2020-08-29 17:25:51 +0100

    updated siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ require(`dotenv`).config()
 module.exports = {
   pathPrefix: "/",
   siteMetadata: {
-    title: "Reflex",
-    description: "Starter for Reflex.",
+    title: "Thornton Pickard",
+    description: "A resource dedicated to Thornton Pickard vintage cameras, equipment and ephemera",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
   plugins: ["@reflexjs/gatsby-theme-base"],
